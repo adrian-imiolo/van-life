d876d8756b8b9e3d7d9db23915ed99c954f91cfc
Adrian Imioło <adrian.imiolo11@gmail.com>
2023-07-27 19:52:59 +0200

    Dashboard and HostVanDetail update

--- api.js
+++ api.js
@@ -5,6 +5,8 @@ import {
   getDocs,
   getDoc,
   doc,
+  query,
+  where,
 } from "firebase/firestore/lite";
 const firebaseConfig = {
   apiKey: "AIzaSyCQrROZx6Wt5B_hLtkHfG6ZshcV3cozMSw",
@@ -40,8 +42,17 @@ export async function getVan(id) {
   };
 }
 
-// export async function getVans(id) {
-//   const url = id ? `/api/vans/${id}` : "/api/vans";
+export async function getHostVans() {
+  const q = query(vansCollectionRef, where("hostId", "==", "123"));
+  const snapshot = await getDocs(q);
+  const vans = snapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+  return vans;
+}
+// export async function getHostVans(id) {
+//   const url = id ? `/api/host/vans/${id}` : "/api/host/vans";
 //   const res = await fetch(url);
 //   if (!res.ok) {
 //     throw {
@@ -54,34 +65,20 @@ export async function getVan(id) {
 //   return data.vans;
 // }
 
-export async function getHostVans(id) {
-  const url = id ? `/api/host/vans/${id}` : "/api/host/vans";
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw {
-      message: "Failed to fetch vans",
-      statusText: res.statusText,
-      status: res.status,
-    };
-  }
-  const data = await res.json();
-  return data.vans;
-}
+// export async function loginUser(creds) {
+//   const res = await fetch("/api/login", {
+//     method: "post",
+//     body: JSON.stringify(creds),
+//   });
+//   const data = await res.json();
 
-export async function loginUser(creds) {
-  const res = await fetch("/api/login", {
-    method: "post",
-    body: JSON.stringify(creds),
-  });
-  const data = await res.json();
+//   if (!res.ok) {
+//     throw {
+//       message: data.message,
+//       statusText: res.statusText,
+//       status: res.status,
+//     };
+//   }
 
-  if (!res.ok) {
-    throw {
-      message: data.message,
-      statusText: res.statusText,
-      status: res.status,
-    };
-  }
-
-  return data;
-}
+//   return data;
+// }
